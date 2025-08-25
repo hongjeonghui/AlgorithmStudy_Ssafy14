2eefcad1d92cfa30e84e892fefcb6e76e26607b4
daeyeonkkk <138367487+daeyeonkkk@users.noreply.github.com>
2025-08-25 12:06:58 +0900

    Delete 10810_ball_nuhki.py

--- 10810_ball_nuhki.py
+++
@@ -1,8 +0,0 @@
-N, M = map(int, input().split())
-bucket = [0] * N
-for _ in range(M):
-    i, j, k = map(int, input().split())
-    for num in range(i-1, j):
-        bucket[num] = k
-print(*bucket)
-
