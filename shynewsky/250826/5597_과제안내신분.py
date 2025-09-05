cbc476d369d9ce0970de4996baa24afa3da78fca
ZeroDivision <112185343+shynewsky@users.noreply.github.com>
2025-09-05 10:18:49 +0900

    feat: [week4] 새하 - 5597번 문제 풀이

---
+++ shynewsky/250826/5597_과제안내신분.py
@@ -0,0 +1,9 @@
+import sys
+sys.stdin = open('input.txt')
+
+students = [i+1 for i in range(30)]
+projects = [int(input()) for _ in range(28)]
+
+for student in students:
+    if student not in projects:
+        print(student)
