d1f28d8e30c5f2539ee5bfb447658a81436ab51c
ZeroDivision <112185343+shynewsky@users.noreply.github.com>
2025-09-05 10:16:09 +0900

    feat: [week4] 새하 - 10810번 문제 풀이

---
+++ shynewsky/250825/10810_공넣기.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin = open('input.txt')
+
+N, M = map(int, input().split())
+matrix = [list(map(int, input().split())) for _ in range(M)]
+
+baguni = [0] * N
+for i,j,k in matrix:
+    for kan in range(i-1, j):
+        baguni[kan] = k
+
+print(*baguni)
